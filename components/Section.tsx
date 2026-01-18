b64e2d64c52af81f506a41482030faad420691b5
Mario Shaya <h2beatz13@gmail.com>
2026-01-18 16:58:46 +0300

    Refactor Section component to improve layout structure by nesting children within a div for better styling and responsiveness

--- components/Section.tsx
+++ components/Section.tsx
@@ -2,8 +2,10 @@ import type { ReactNode } from "react"
 
 const Section = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="mx-auto min-h-dvh flex max-w-7xl flex-col items-center justify-center gap-4 px-4 sm:px-6 not-last:border-b">
-      {children}
+    <section className="not-last:border-b border-white/10 w-full">
+      <div className="mx-auto flex flex-col items-center justify-center gap-4 max-w-7xl min-h-dvh px-4 sm:px-6">
+        {children}
+      </div>
     </section>
   )
 }
