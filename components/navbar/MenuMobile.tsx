03d41d50f76c0bae0efc67cf5db16e375252a787
Mario Shaya <h2beatz13@gmail.com>
2026-01-18 12:09:11 +0300

    Add onClick handler to MenuMobile component links for improved interactivity

--- components/navbar/MenuMobile.tsx
+++ components/navbar/MenuMobile.tsx
@@ -58,6 +58,7 @@ const MenuMobile = ({ onClick }: MenuMobileProps) => {
             )}
             href={item.link}
             key={item.link}
+            onClick={onClick}
           >
             {item.label}
           </Link>
