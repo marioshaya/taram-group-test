b961169dd795a51ce19e569cf8a70d289d1b208d
Mario Shaya <h2beatz13@gmail.com>
2026-01-18 14:59:51 +0300

    Update Home component layout to include multiple LandingSection instances and adjust Section component for minimum height

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ import LandingSection from "@/components/landing"
 
 export default function Home() {
   return (
-    <main className="bg-background flex min-h-dvh items-center justify-center">
+    <main className="bg-background flex flex-col  items-center justify-center">
+      <LandingSection />
       <LandingSection />
     </main>
   )
--- components/Section.tsx
+++ components/Section.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from "react"
 
 const Section = ({ children }: { children: ReactNode }) => {
   return (
-    <section className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-4 px-4 sm:px-6 not-last:border-b">
+    <section className="mx-auto min-h-dvh flex max-w-7xl flex-col items-center justify-center gap-4 px-4 sm:px-6 not-last:border-b">
       {children}
     </section>
   )
