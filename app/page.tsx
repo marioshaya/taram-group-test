f00b12184b80bb7e88c429b5094682ef517a4f9e
Mario Shaya <h2beatz13@gmail.com>
2026-01-18 08:54:04 +0300

    added prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-import Image from "next/image";
-import { FaCalendarAlt } from "react-icons/fa";
-import { IoMdArrowRoundDown } from "react-icons/io";
+import Image from "next/image"
+import { FaCalendarAlt } from "react-icons/fa"
+import { IoMdArrowRoundDown } from "react-icons/io"
 
 export default function Home() {
   return (
-    <main className="flex min-h-dvh items-center justify-center bg-background">
+    <main className="bg-background flex min-h-dvh items-center justify-center">
       <div className="flex-1">
-        <div className="mx-auto max-w-7xl flex flex-col items-center justify-center gap-4 px-4">
-          <div className="flex items-center justify-center bg-white rounded-full px-8 py-3 text-background shadow-[0_18px_48px_rgba(0,0,0,0.32),0_0_20px_rgba(113,221,174,0.4),0_0_20px_rgba(113,221,174,0.2)]">
+        <div className="mx-auto flex max-w-7xl flex-col items-center justify-center gap-4 px-4">
+          <div className="text-background flex items-center justify-center rounded-full bg-white px-8 py-3 shadow-[0_18px_48px_rgba(0,0,0,0.32),0_0_20px_rgba(113,221,174,0.4),0_0_20px_rgba(113,221,174,0.2)]">
             <Image src="/search.png" height={48} width={48} alt="Logo" />
-            <div className="uppercase font-extrabold text-sm lg:text-2xl overflow-hidden text-ellipsis">
+            <div className="overflow-hidden text-sm font-extrabold text-ellipsis uppercase lg:text-2xl">
               Créer un site web <u>vraiement</u> unique
             </div>
           </div>
 
-          <h1 className="max-w-3xl text-3xl lg:text-5xl font-semibold tracking-tight text-center">
+          <h1 className="max-w-3xl text-center text-3xl font-semibold tracking-tight lg:text-5xl">
             Votre <span className="text-primary">site</span> doit{" "}
             <span className="text-primary">donner envie</span> de{" "}
             <span className="text-primary">rester</span>, pas de{" "}
@@ -26,27 +26,27 @@ export default function Home() {
             .
           </h1>
 
-          <p className="text-center max-w-lg text-base font-semibold text-white/80">
+          <p className="max-w-lg text-center text-base font-semibold text-white/80">
             Design moderne, solide SEO, Suivi complet : <br />
             on construit un site qui retient vos visiteurs et vous apporte des
             résultats.
           </p>
 
-          <div className="flex font-extrabold flex-col md:flex-row items-center justify-center gap-4">
-            <div className="w-full md:w-auto relative">
+          <div className="flex flex-col items-center justify-center gap-4 font-extrabold md:flex-row">
+            <div className="relative w-full md:w-auto">
               <button
-                className="w-full md:w-auto flex items-center text-background justify-center gap-x-2 bg-linear-to-r from-primary hover:cursor-pointer to-[#2A9D7A] hover:duration-150 hover:scale-105 ease-in-out transition-all rounded-xl px-4 py-2"
+                className="text-background from-primary flex w-full items-center justify-center gap-x-2 rounded-xl bg-linear-to-r to-[#2A9D7A] px-4 py-2 transition-all ease-in-out hover:scale-105 hover:cursor-pointer hover:duration-150 md:w-auto"
                 type="button"
               >
                 <FaCalendarAlt />
                 <div>Prendre RDV</div>
               </button>
-              <div className="absolute hidden md:block -bottom-12 -left-10">
-                <Image src="/click.png" height={128} width={128} alt="click"/>
+              <div className="absolute -bottom-12 -left-10 hidden md:block">
+                <Image src="/click.png" height={128} width={128} alt="click" />
               </div>
             </div>
             <button
-              className="w-full sm:w-auto flex items-center justify-center gap-x-2 rounded-xl px-4 py-2 border text-primary hover:cursor-pointer hover:duration-150 hover:scale-105 ease-in-out transition-all"
+              className="text-primary flex w-full items-center justify-center gap-x-2 rounded-xl border px-4 py-2 transition-all ease-in-out hover:scale-105 hover:cursor-pointer hover:duration-150 sm:w-auto"
               type="button"
             >
               <div>Découvrir nos projets</div>
@@ -56,5 +56,5 @@ export default function Home() {
         </div>
       </div>
     </main>
-  );
+  )
 }
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,24 +1,26 @@
-import Image from "next/image";
-import Link from "next/link";
-import { MdMenu } from "react-icons/md";
-import { FaCalendarAlt } from "react-icons/fa";
+import Image from "next/image"
+import Link from "next/link"
+import { MdMenu } from "react-icons/md"
+import { FaCalendarAlt } from "react-icons/fa"
 
 export default function Navbar() {
-  return <header className="fixed top-0 left-0 w-full bg-primary">
-    <div className="flex items-center justify-between h-20 w-full mx-auto px-6">
-      <button className="text-2xl text-background h-10 w-10" type="button">
-        <MdMenu />
-      </button>
-      <Link href="/">
-        <Image src="/logo-full.png" width={100} height={100} alt="Logo" />
-      </Link>
-      <button 
-        aria-label="Prendre rendez-vous"
-        className="bg-background h-12 w-12 flex items-center justify-center rounded-xl" 
-        type="button"
-      >
-        <FaCalendarAlt />
-      </button>
-    </div>
-  </header>
+  return (
+    <header className="bg-primary fixed top-0 left-0 w-full">
+      <div className="mx-auto flex h-20 w-full items-center justify-between px-6">
+        <button className="text-background h-10 w-10 text-2xl" type="button">
+          <MdMenu />
+        </button>
+        <Link href="/">
+          <Image src="/logo-full.png" width={100} height={100} alt="Logo" />
+        </Link>
+        <button
+          aria-label="Prendre rendez-vous"
+          className="bg-background flex h-12 w-12 items-center justify-center rounded-xl"
+          type="button"
+        >
+          <FaCalendarAlt />
+        </button>
+      </div>
+    </header>
+  )
 }
