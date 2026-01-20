878196dcf4f381ee8cbd5abeff26573ba755a0e7
Mario Shaya <h2beatz13@gmail.com>
2026-01-20 19:32:59 +0300

    Update NeedSection layout by modifying the container to use flexbox for improved alignment and responsiveness.

--- components/landing/NeedSection.tsx
+++ components/landing/NeedSection.tsx
@@ -32,7 +32,7 @@ export default function NeedSection() {
               alt="Conception web"
             />
           </div>
-          <div className="w-full relative">
+          <div className="w-full flex flex-col items-center justify-center relative">
             {!activeNeed && (
               <div className="w-full h-full">
                 <h4 className="text-2xl text-primary font-extrabold py-4 md:pt-0">
