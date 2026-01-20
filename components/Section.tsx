07991f9699444c50c400cc31b617d1d5f6f8a5be
Mario Shaya <h2beatz13@gmail.com>
2026-01-20 20:33:42 +0300

    Update Section component to set minimum height for improved layout consistency and responsiveness.

--- components/Section.tsx
+++ components/Section.tsx
@@ -13,7 +13,7 @@ const Section = ({
       <div
         className={clsx(
           !isNotMaxWidth && "max-w-7xl",
-          "mx-auto flex flex-col items-center justify-center gap-4 py-4 px-4 sm:px-6"
+          "mx-auto flex flex-col items-center justify-center min-h-screen gap-4 py-4 px-4 sm:px-6"
         )}
       >
         {children}
