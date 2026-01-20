8690c36294b6f8d43d79c52f7ad8f58c27d24633
Mario Shaya <h2beatz13@gmail.com>
2026-01-20 16:25:30 +0300

    Add IDs to needsData entries for better identification and management

--- data/need.ts
+++ data/need.ts
@@ -4,14 +4,17 @@ import { RiRobot2Fill } from "react-icons/ri"
 
 export const needsData = [
   {
+    id: "web",
     title: "Un site web",
     icon: CiGlobe
   },
   {
+    id: "mobile",
     title: "Une application mobile",
     icon: MdOutlinePhoneIphone
   },
   {
+    id: "automation",
     title: "De l'automatisation",
     icon: RiRobot2Fill
   }
