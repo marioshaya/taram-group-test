d48336d449e91f44366ad667fab7cf0ad854639b
Mario Shaya <h2beatz13@gmail.com>
2026-01-23 19:14:01 +0300

    Refactor BookAFreeCall component to use BookAFreeCallProps interface for improved type management and consistency. This change enhances code readability and aligns with the updated props structure.

--- components/BookAFreeCall.tsx
+++ components/BookAFreeCall.tsx
@@ -1,13 +1,8 @@
+import type { BookAFreeCallProps } from "@/types"
 import BackBtn from "./buttons/BackBtn"
 import OfferCard from "./cards/OfferCard"
 
-interface BookFreeCallIO {
-  title: string
-  paragraph: string
-  onClick: () => void
-}
-
-const BookAFreeCall = ({ title, paragraph, onClick }: BookFreeCallIO) => {
+const BookAFreeCall = ({ title, paragraph, onClick }: BookAFreeCallProps) => {
   return (
     <div className="w-full h-full flex flex-col justify-center gap-y-4 bg-background">
       <div className="flex flex-col md:flex-row md:items-center justify-between">
--- types/props.ts
+++ types/props.ts
@@ -2,3 +2,9 @@ export interface OfferCardProps {
   title: string
   paragraph: string
 }
+
+export interface BookAFreeCallProps {
+  title: string
+  paragraph: string
+  onClick: () => void
+}
