1f25e16c120c0d64f9ebe125e139e0d1155bd19b
Mario Shaya <h2beatz13@gmail.com>
2026-01-23 19:47:58 +0300

    Add OfferCard component to OffresPage for dynamic offer display and enhance layout with offers data integration

--- app/offres/page.tsx
+++ app/offres/page.tsx
@@ -1,5 +1,7 @@
+import OfferCard from "@/components/cards/OfferCard"
 import NeedSection from "@/components/landing/NeedSection"
 import Section from "@/components/section/Section"
+import { offers } from "@/data"
 
 export default function OffresPage() {
   return (
@@ -10,6 +12,15 @@ export default function OffresPage() {
           Toutes nos{" "}
           <span className="text-4xl sm:text-4xl text-primary">offres</span>
         </h3>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {offers.map((off) => (
+            <OfferCard
+              paragraph={off.paragraph}
+              title={off.title}
+              key={off.title}
+            />
+          ))}
+        </div>
       </Section>
     </main>
   )
--- components/cards/OfferCard.tsx
+++ components/cards/OfferCard.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Image from "next/image"
 import { FaCalendarAlt } from "react-icons/fa"
 import { LiaLongArrowAltRightSolid } from "react-icons/lia"
